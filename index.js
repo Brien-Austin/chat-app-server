89b06de0e97359d3c9ab6cffb8efc7a6402c6633
Brien-Austin <brienaustinclayton@gmail.com>
2024-07-18 23:13:25 +0530

    removed admin

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { db } = require('./config/database');
-const adminRoutes = require('./routes/admin/adminRoutes')
+
 const userRoutes = require('./routes/user/userRoutes')
 dotenv.config()
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/admin',adminRoutes)
+
 app.use('/api/auth',userRoutes)
 
 app.listen(process.env.PORT, ()=>{
--- routes/admin/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const adminController = require("../../controller/admin/adminController");
-
-const router = express.Router();
-
-router.post("/create-course", adminController.createCourse);
-router.post("/create-chapters", adminController.createChapter)
-router.get("/courses", adminController.getAllCourses);
-module.exports = router;
